91a9aee8344f20ebdb52eefeffaaa240dd142652
zunvu <vudung.siaedu@gmail.com>
2021-12-19 01:29:21 +0700

    initial commit

---
+++ scripts/start-relayer-polygon.sh
@@ -0,0 +1,4 @@
+echo Starting relayer...
+chainbridge --config config/config.json --latest
+
+echo Relayer started
---
+++ scripts/stop-docker-substrate.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker-compose -f ../docker-compose-snippet.yml down -v
